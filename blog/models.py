85d8e758a9120736f02d4366388651c61c474375
xumingrui-kyomeiei <2020m818@hokuriku-u.ac.jp>
2020-12-06 22:50:07 +0900

    Modified templates to display

--- blog/models.py
+++ blog/models.py
@@ -7,12 +7,12 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(blank=True,null=True)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
         self.publish_date = timezone.now()
         self.save()
 
-    def _str_(self):
+    def __str__(self):
         return self.title
